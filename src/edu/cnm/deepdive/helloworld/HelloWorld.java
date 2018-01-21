dd9fb83a95f80178baee458c8b557451407a240b
Donny Sanchez <Admin@10.0.0.17>
2018-01-21 16:34:37 -0700

    New project file implementing "Hello World" program.

---
+++ src/edu/cnm/deepdive/helloworld/HelloWorld.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+package edu.cnm.deepdive.helloworld;
+
+/**
+ * @author Admin
+ *
+ */
+public class HelloWorld {
+
+  /**
+   * Prints "Hello World!" to standard output.
+   * @param args    Command-line parameters (ignored).
+   */
+  public static void main(String[] args) {
+    System.out.println("Hello World!");
+
+  }
+
+}
